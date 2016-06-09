6b19bafe47e6f11364403957854e434e76b0be4c
VadimTk <vadim@percona.com>
2016-06-09 17:05:29 -0400

    Percona XtraDB Cluster images

---
+++ pxc-57/misc/startetcd.sh
@@ -0,0 +1,11 @@
+ETCD_HOST=${ETCD_HOST:-10.20.2.4:2379}
+docker run -d -v /usr/share/ca-certificates/:/etc/ssl/certs -p 4001:4001 -p 2380:2380 -p 2379:2379 \
+ --name etcd quay.io/coreos/etcd \
+ -name etcd0 \
+ -advertise-client-urls http://${ETCD_HOST}:2379,http://${ETCD_HOST}:4001 \
+ -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
+ -initial-advertise-peer-urls http://${ETCD_HOST}:2380 \
+ -listen-peer-urls http://0.0.0.0:2380 \
+ -initial-cluster-token etcd-cluster-1 \
+ -initial-cluster etcd0=http://${ETCD_HOST}:2380 \
+ -initial-cluster-state new
