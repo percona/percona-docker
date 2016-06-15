b346622424b42f9090d4b159db471fbd5084f7bf
VadimTk <vadim@percona.com>
2016-06-15 01:55:54 -0400

    percona-server-master-slave

---
+++ percona-server-master-slave/start_slave.sh
@@ -0,0 +1,15 @@
+REPLICASET_NAME=${REPLICASET_NAME:-replset1}
+NODE_NAME=${NODE_NAME:-${REPLICASET_NAME}_slave}
+NETWORK_NAME=${REPLICASET_NAME}_net
+
+docker network create -d overlay $NETWORK_NAME
+
+echo "Starting new node..."
+docker run -d -p 3306 --net=$NETWORK_NAME \
+         --name=${NODE_NAME} \
+	 -e MYSQL_ROOT_PASSWORD=Theistareyk \
+	 -e MASTER_HOST=${REPLICASET_NAME}_master \
+	 perconalab/ps-master-slave
+#--general-log=1 --general_log_file=/var/lib/mysql/general.log
+echo "Started $(docker ps -l -q)"
+
