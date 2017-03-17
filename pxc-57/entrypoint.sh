bc5ba631a10cd019c3f5abdbf44d72cf8e040496
VadimTK <vadim@percona.com>
2017-03-17 16:46:55 -0400

    PXC adjustments

--- pxc-57/entrypoint.sh
+++ pxc-57/entrypoint.sh
@@ -14,7 +14,7 @@ fi
 	# Get config
 	DATADIR="$("mysqld" --verbose --wsrep_provider= --help 2>/dev/null | awk '$1 == "datadir" { print $2; exit }')"
 
-	if [ ! -e "$DATADIR/init.ok" ]; then
+	if [ ! -e "$DATADIR/mysql" ]; then
 		if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" -a -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
                         echo >&2 'error: database is uninitialized and password option is not specified '
                         echo >&2 '  You need to specify one of MYSQL_ROOT_PASSWORD, MYSQL_ALLOW_EMPTY_PASSWORD and MYSQL_RANDOM_ROOT_PASSWORD'
@@ -100,7 +100,6 @@ fi
 		echo
 		#mv /etc/my.cnf $DATADIR
 	fi
-	touch $DATADIR/init.ok
 	chown -R mysql:mysql "$DATADIR"
 
 if [ -z "$DISCOVERY_SERVICE" ]; then
