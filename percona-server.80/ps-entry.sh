aa0b7822f8a456bebc789dafb2202ab9651f64df
Bleza Takouda <dreambeam77@gmail.com>
2019-01-04 13:48:53 +0900

    Fix entryscrypt
    mysql.infoschema and mysql.session users are missing after installation
    ```
    mysql> show databases;
    ERROR 1449 (HY000): The user specified as a definer ('mysql.infoschema'@'localhost') does not exist
    ```

--- percona-server.80/ps-entry.sh
+++ percona-server.80/ps-entry.sh
@@ -166,7 +166,7 @@ if [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 			--  or products like mysql-fabric won't work
 			SET @@SESSION.SQL_LOG_BIN=0;
 
-			DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost') ;
+			DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'mysql.infoschema', 'mysql.session', 'root') OR host NOT IN ('localhost') ;
 			ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 			GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION ;
 			${rootCreate}
