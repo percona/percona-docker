5f4a84702fb90cb295bfee7476b0db6ff9ecea5e
Mykola Marzhan <mykola.marzhan@percona.com>
2019-07-02 15:39:25 +0200

    CLOUD-298 merge k8s changes into official proxysql images

---
+++ proxysql/dockerdir/entrypoint.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+set -o xtrace
+
+function mysql_root_exec() {
+  local server="$1"
+  local query="$2"
+  MYSQL_PWD=${MYSQL_ROOT_PASSWORD:-password} timeout 600 mysql -h "${server}" -uroot -s -NB -e "${query}"
+}
+
+function wait_for_mysql() {
+    local h="$1"
+    echo "Waiting for host $h to be online..."
+    while [ "$(mysql_root_exec "$h" 'select 1')" != "1" ]
+    do
+        echo "MySQL is not up yet... sleeping ..."
+        sleep 1
+    done
+}
+
+PROXY_CFG=/etc/proxysql/proxysql.cnf
+PROXY_ADMIN_CFG=/etc/proxysql-admin.cnf
+
+sed "s/\"admin:admin\"/\"${PROXY_ADMIN_USER:-admin}:$PROXY_ADMIN_PASSWORD\"/g"       ${PROXY_CFG} 1<> ${PROXY_CFG}
+sed "s/cluster_username=\"admin\"/cluster_username=\"${PROXY_ADMIN_USER:-admin}\"/g" ${PROXY_CFG} 1<> ${PROXY_CFG}
+sed "s/cluster_password=\"admin\"/cluster_password=\"$PROXY_ADMIN_PASSWORD\"/g"      ${PROXY_CFG} 1<> ${PROXY_CFG}
+sed "s/PROXYSQL_USERNAME='admin'/PROXYSQL_USERNAME='${PROXY_ADMIN_USER:-admin}'/g" ${PROXY_ADMIN_CFG} 1<> ${PROXY_ADMIN_CFG}
+sed "s/PROXYSQL_PASSWORD='admin'/PROXYSQL_PASSWORD='$PROXY_ADMIN_PASSWORD'/g"      ${PROXY_ADMIN_CFG} 1<> ${PROXY_ADMIN_CFG}
+sed "s/CLUSTER_USERNAME='admin'/CLUSTER_USERNAME='root'/g"                         ${PROXY_ADMIN_CFG} 1<> ${PROXY_ADMIN_CFG}
+sed "s/CLUSTER_PASSWORD='admin'/CLUSTER_PASSWORD='$MYSQL_ROOT_PASSWORD'/g"         ${PROXY_ADMIN_CFG} 1<> ${PROXY_ADMIN_CFG}
+sed "s/MONITOR_USERNAME='monitor'/MONITOR_USERNAME='monitor'/g"                    ${PROXY_ADMIN_CFG} 1<> ${PROXY_ADMIN_CFG}
+sed "s/MONITOR_PASSWORD='monitor'/MONITOR_PASSWORD='$MONITOR_PASSWORD'/g"          ${PROXY_ADMIN_CFG} 1<> ${PROXY_ADMIN_CFG}
+
+## SSL/TLS support
+CA=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+if [ -f "/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt" ]; then
+    CA=/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt
+fi
+SSL_DIR=${SSL_DIR:-/etc/proxysql/ssl}
+if [ -f "${SSL_DIR}/ca.crt" ]; then
+    CA=${SSL_DIR}/ca.crt
+fi
+SSL_INTERNAL_DIR=${SSL_INTERNAL_DIR:-/etc/proxysql/ssl-internal}
+if [ -f "${SSL_INTERNAL_DIR}/ca.crt" ]; then
+    CA=${SSL_INTERNAL_DIR}/ca.crt
+fi
+
+KEY=${SSL_DIR}/tls.key
+CERT=${SSL_DIR}/tls.crt
+if [ -f "${SSL_INTERNAL_DIR}/tls.key" ] && [ -f "${SSL_INTERNAL_DIR}/tls.crt" ]; then
+    KEY=${SSL_INTERNAL_DIR}/tls.key
+    CERT=${SSL_INTERNAL_DIR}/tls.crt
+fi
+
+if [ -f "$CA" ] && [ -f "$KEY" ] && [ -f "$CERT" ] && [ -z "$PXC_SERVICE" ]; then
+    wait_for_mysql "$PXC_SERVICE"
+    cipher=$(mysql_root_exec "$PXC_SERVICE" 'SHOW SESSION STATUS LIKE "Ssl_cipher"' | awk '{print$2}')
+
+    sed "s^have_ssl=false^have_ssl=true^"                   ${PROXY_CFG} 1<> ${PROXY_CFG}
+    sed "s^ssl_p2s_ca=\"\"^ssl_p2s_ca=\"$CA\"^"             ${PROXY_CFG} 1<> ${PROXY_CFG}
+    sed "s^ssl_p2s_ca=\"\"^ssl_p2s_ca=\"$CA\"^"             ${PROXY_CFG} 1<> ${PROXY_CFG}
+    sed "s^ssl_p2s_key=\"\"^ssl_p2s_key=\"$KEY\"^"          ${PROXY_CFG} 1<> ${PROXY_CFG}
+    sed "s^ssl_p2s_cert=\"\"^ssl_p2s_cert=\"$CERT\"^"       ${PROXY_CFG} 1<> ${PROXY_CFG}
+    sed "s^ssl_p2s_cipher=\"\"^ssl_p2s_cipher=\"$cipher\"^" ${PROXY_CFG} 1<> ${PROXY_CFG}
+fi
+
+if [ -f "${SSL_DIR}/tls.key" ] && [ -f "${SSL_DIR}/tls.crt" ]; then
+    cp "${SSL_DIR}/tls.key" /var/lib/proxysql/proxysql-key.pem
+    cp "${SSL_DIR}/tls.crt" /var/lib/proxysql/proxysql-cert.pem
+fi
+if [ -f "${SSL_DIR}/ca.crt" ]; then
+    cp "${SSL_DIR}/ca.crt" /var/lib/proxysql/proxysql-ca.pem
+fi
+
+exec "$@"
---
+++ proxysql/dockerdir/usr/bin/add_proxysql_nodes.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -o errexit
+set -o xtrace
+
+function proxysql_admin_exec() {
+  local server="$1"
+  local query="$2"
+  MYSQL_PWD="${PROXY_ADMIN_PASSWORD:-admin}" timeout 600 \
+      mysql -h "${server}" -P "${PROXY_ADMIN_PORT:-6032}" -u "${PROXY_ADMIN_USER:-admin}" -s -NB -e "${query}"
+}
+
+function wait_for_proxysql() {
+    local server="$1"
+    echo "Waiting for host $server to be online..."
+    while [ "$(proxysql_admin_exec "$server" 'select 1')" != "1" ]
+    do
+        echo "ProxySQL is not up yet... sleeping ..."
+        sleep 1
+    done
+}
+
+add_proxysql() {
+    local dest=$1
+    local host=$2
+    proxysql_admin_exec "$dest" "
+        INSERT INTO proxysql_servers (hostname,port) VALUES ('$host','${PROXY_ADMIN_PORT:-6032}');
+    "
+}
+
+function main() {
+    echo "Running $0"
+    wait_for_proxysql "127.0.0.1"
+
+    proxysql_admin_exec "127.0.0.1" "
+        DELETE FROM proxysql_servers;
+    "
+    while read -ra LINE; do
+        echo "Read line $LINE"
+        add_proxysql "127.0.0.1" "$LINE"
+    done
+    proxysql_admin_exec "127.0.0.1" "
+        SELECT * FROM proxysql_servers;
+        LOAD PROXYSQL SERVERS TO RUNTIME;
+        SAVE PROXYSQL SERVERS TO DISK;
+    "
+
+    echo "All done!"
+}
+
+main
+exit 0
