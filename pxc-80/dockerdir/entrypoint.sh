d77224ff22af6cfc0723f0141f11fc5cb5c23c26
paval <pavel.kasko@percona.com>
2020-01-14 14:23:26 +0200

    CLOUD-429: fix user deleting

--- pxc-80/dockerdir/entrypoint.sh
+++ pxc-80/dockerdir/entrypoint.sh
@@ -218,7 +218,7 @@ if [ -z "$CLUSTER_JOIN" ] && [ "$1" = 'mysqld' -a -z "$wantHelp" ]; then
 			--  or products like mysql-fabric won't work
 			SET @@SESSION.SQL_LOG_BIN=0;
 
-			DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysqlxsys', 'root') OR host NOT IN ('localhost') ;
+			-- DELETE FROM mysql.user WHERE user NOT IN ('mysql.sys', 'mysql.infoschema', 'mysql.pxc.internal.session', 'mysql.pxc.sst.role', 'mysql.pxc.sst.user', 'mysql.session', 'mysqlxsys', 'root') ;
 			ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' ;
 			GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION ;
 			${rootCreate}
