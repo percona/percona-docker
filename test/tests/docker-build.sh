c6bb2dbfccd5897fbfdb5a3b823b5fae30601e3f
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-01-20 15:14:23 +0200

    ENG-184 Improve mongodb docker images

--- test/tests/docker-build.sh
+++ test/tests/docker-build.sh
@@ -18,7 +18,7 @@ dir="$1"; shift
 
 imageTag="$1"; shift
 
-tmp="$(mktemp -d -t docker-library-test-build-XXXXXXXXXX)"
+tmp="$(mktemp -t -d docker-library-test-build-XXXXXXXXXX)"
 trap "rm -rf '$tmp'" EXIT
 
 cat > "$tmp/Dockerfile"
@@ -26,16 +26,6 @@ cat > "$tmp/Dockerfile"
 from="$(awk -F '[ \t]+' 'toupper($1) == "FROM" { print $2; exit }' "$tmp/Dockerfile")"
 if ! docker inspect "$from" &> /dev/null; then
 	docker pull "$from" > /dev/null
-fi
-
-onbuilds="$(docker inspect -f '{{len .Config.OnBuild}}' "$from")"
-if [ "$onbuilds" -gt 0 ]; then
-	# crap, the image we want to build has some ONBUILD instructions
-	# those are kind of going to ruin our day
-	# let's do some hacks to strip those bad boys out in a new fake layer
-	"$(dirname "$(realpath "$BASH_SOURCE")")/remove-onbuild.sh" "$from" "$imageTag"
-	awk -F '[ \t]+' 'toupper($1) == "FROM" { $2 = "'"$imageTag"'" } { print }' "$tmp/Dockerfile" > "$tmp/Dockerfile.new"
-	mv "$tmp/Dockerfile.new" "$tmp/Dockerfile"
 fi
 
 cp -RL "$dir" "$tmp/dir"
