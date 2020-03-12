d1a699589d003cdad3d902163dc6c80374546eb5
paval <pavel.kasko@percona.com>
2020-03-12 09:45:06 +0200

    Restore using transition key

--- pxc-80-backup/recovery-pvc-joiner.sh
+++ pxc-80-backup/recovery-pvc-joiner.sh
@@ -30,8 +30,33 @@ function check_ssl() {
     fi
 }
 
+parse_sst_info() {
+   local source_path=$1
+   local group=$2
+   local var=$3
+   local reval=""
+
+   reval=$(my_print_defaults -c "$source_path" $group | awk -F= '{if ($1 ~ /_/) { gsub(/_/,"-",$1); print $1"="$2 } else { print $0 }}' | grep -- "--$var=" | cut -d= -f2- | tail -1)
+
+   # use default if we haven't found a value
+   if [[ -z $reval ]]; then
+      [[ -n $4 ]] && reval=$4
+   fi
+
+   echo $reval
+}
+
 ping -c1 $RESTORE_SRC_SERVICE || :
 rm -rf /datadir/*
+
 socat -u "$SOCAT_OPTS" stdio | xbstream -x -C /datadir --parallel=$(grep -c processor /proc/cpuinfo)
 socat -u "$SOCAT_OPTS" stdio | xbstream -x -C /datadir --parallel=$(grep -c processor /proc/cpuinfo)
-xtrabackup --prepare --target-dir=/datadir ${XB_USE_MEMORY+--use-memory=$XB_USE_MEMORY}
+
+# xtrabackup --use-memory=750000000 --prepare --binlog-info=ON '--transition-key=$transition_key' --rollback-prepared-trx --xtrabackup-plugin-dir=/usr/bin/pxc_extra/pxb-8.0/lib/plugin --target-dir=/var/lib/mysql//sst-xb-tmpdir
+transition_key=$(parse_sst_info "/datadir/sst_info" sst transition-key "")
+if [[ -n $transition_key ]]; then
+    (xtrabackup --prepare --binlog-info=ON --rollback-prepared-trx --target-dir=/datadir --xtrabackup-plugin-dir=/usr/lib64/xtrabackup/plugin --transition-key=$transition_key ${XB_USE_MEMORY+--use-memory=$XB_USE_MEMORY}) || sleep infinity
+else
+    echo "failed to parse transition key"
+    exit 1
+fi
--- pxc-80/dockerdir/entrypoint.sh
+++ pxc-80/dockerdir/entrypoint.sh
@@ -297,6 +297,7 @@ keyring="/var/lib/mysql-keyrings/$(hostname)"
 if [ -f "$keyring" ]; then
 	sed -i "/\[mysqld\]/a keyring_file_data=$keyring" $CFG
 	sed -i "/\[mysqld\]/a default_table_encryption=1" $CFG
+	sed -i "/\[mysqld\]/a early-plugin-load=keyring_file.so" $CFG
 	content="$(cat "$keyring")"
 	id=${content:38:36}
 	DATADIR="$(grep "datadir=" $CFG | sed "s/datadir=//")"
