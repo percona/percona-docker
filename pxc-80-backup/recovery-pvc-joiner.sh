b429e9dc6035a49ec368f9e64794998f33b45a5b
paval <pavel.kasko@percona.com>
2020-03-23 17:39:34 +0200

    Put/get transition key to vault

--- pxc-80-backup/recovery-pvc-joiner.sh
+++ pxc-80-backup/recovery-pvc-joiner.sh
@@ -3,6 +3,9 @@
 set -o errexit
 set -o xtrace
 
+pwd=$(realpath $(dirname $0))
+. ${pwd}/vault-get-key.sh
+
 SOCAT_OPTS="TCP:${RESTORE_SRC_SERVICE}:3307,retry=30"
 function check_ssl() {
     CA=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
@@ -30,19 +33,14 @@ function check_ssl() {
     fi
 }
 
-parse_sst_info() {
-   local source_path=$1
-   awk -F "=" '/transition-key/ {print $2}' "$source_path"
-}
-
 check_ssl
 ping -c1 $RESTORE_SRC_SERVICE || :
 rm -rf /datadir/*
 
-socat -u "$SOCAT_OPTS" stdio | xbstream -x -C /datadir --parallel=$(grep -c processor /proc/cpuinfo)
+socat -u "$SOCAT_OPTS" stdio > /datadir/sst_info
 socat -u "$SOCAT_OPTS" stdio | xbstream -x -C /datadir --parallel=$(grep -c processor /proc/cpuinfo)
 
-transition_key=$(parse_sst_info "/datadir/sst_info")
+transition_key=$(vault_get)
 if [[ -n $transition_key ]]; then
     encrypt_prepare_options="--transition-key=\$transition_key"
 fi
