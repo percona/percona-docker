1dc602595649b63689a5f2814b921e357bebac38
paval <pavel.kasko@percona.com>
2020-03-24 12:35:28 +0200

    DARE for PXC5.7

--- pxc-57-backup/recovery-pvc-joiner.sh
+++ pxc-57-backup/recovery-pvc-joiner.sh
@@ -3,6 +3,9 @@
 set -o errexit
 set -o xtrace
 
+pwd=$(realpath $(dirname $0))
+. ${pwd}/vault.sh
+
 SOCAT_OPTS="TCP:${RESTORE_SRC_SERVICE}:3307,retry=30"
 function check_ssl() {
     CA=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
@@ -30,8 +33,15 @@ function check_ssl() {
     fi
 }
 
+check_ssl
 ping -c1 $RESTORE_SRC_SERVICE || :
 rm -rf /datadir/*
-socat -u "$SOCAT_OPTS" stdio \
-    | xbstream -x -C /datadir --parallel=$(grep -c processor /proc/cpuinfo)
-xtrabackup --prepare --target-dir=/datadir ${XB_USE_MEMORY+--use-memory=$XB_USE_MEMORY}
+
+socat -u "$SOCAT_OPTS" stdio >/datadir/sst_info
+socat -u "$SOCAT_OPTS" stdio | xbstream -x -C /datadir --parallel=$(grep -c processor /proc/cpuinfo)
+
+transition_key=$(vault_get /datadir/sst_info)
+if [[ -n $transition_key ]]; then
+    encrypt_prepare_options="--transition-key=\$transition_key"
+fi
+xtrabackup ${XB_USE_MEMORY+--use-memory=$XB_USE_MEMORY} --prepare --binlog-info=ON $encrypt_prepare_options --rollback-prepared-trx --xtrabackup-plugin-dir=/usr/lib64/xtrabackup/plugin --target-dir=/datadir
