a58ab066189d9da125f423c8bdb17a5eee0a06bb
Mykola Marzhan <mykola.marzhan@percona.com>
2020-03-25 17:04:15 +0200

    K8SPXC-172 use curl for vault operations

--- pxc-80-backup/vault.sh
+++ pxc-80-backup/vault.sh
@@ -2,64 +2,71 @@
 
 set -o errexit
 
-function parse_ini() {
-  local key=$1
-  local file_path=$2
+keyring_vault=/etc/mysql/vault-keyring-secret/keyring_vault.conf
 
-  awk -F "=[ ]*" "/${key}[ ]*=/ {print \$2}" "$file_path"
+function parse_ini() {
+    local key=$1
+    local file_path=$2
+
+    awk -F "=[ ]*" "/${key}[ ]*=/ {print \$2}" "$file_path"
 }
 
 function vault_get() {
-  local sst_info=$1
-  local keyring_vault=/etc/mysql/vault-keyring-secret/keyring_vault.conf
+    local sst_info=$1
 
-  if [ ! -f $keyring_vault ]; then
-    return 0
-  fi
+    if [ ! -f "${keyring_vault}" ]; then
+        return 0
+    fi
 
-  if [ ! -f $sst_info ]; then
-    echo "SST info not found"
-    exit 1
-  fi
+    if [ ! -f "${sst_info}" ]; then
+        echo "SST info not found"
+        exit 1
+    fi
 
-  export VAULT_TOKEN=$(parse_ini "token" "${keyring_vault}")
-  export VAULT_ADDR=$(parse_ini "vault_url" "${keyring_vault}")
-  local vault_root=$(parse_ini "secret_mount_point" "${keyring_vault}")/backup
-  local gtid=$(parse_ini "galera-gtid" "${sst_info}")
+    export VAULT_TOKEN=$(parse_ini "token" "${keyring_vault}")
+    export VAULT_ADDR=$(parse_ini "vault_url" "${keyring_vault}")
+    local vault_root=$(parse_ini "secret_mount_point" "${keyring_vault}")/backup
+    local gtid=$(parse_ini "galera-gtid" "${sst_info}")
 
-  vault kv get "${vault_root}/${gtid}" \
-      | grep transition_key \
-      | sed -e 's/transition_key[[:space:]]*//g'
+    curl \
+        -H "X-Vault-Request: true" \
+        -H "X-Vault-Token: ${VAULT_TOKEN}" \
+        "${VAULT_ADDR}/v1/${vault_root}/backup/${gtid}" \
+        | jq -r '.data.transition_key'
 }
 
 function vault_store() {
-  local sst_info=$1
-  local keyring_vault=/etc/mysql/vault-keyring-secret/keyring_vault.conf
+    local sst_info=$1
 
-  if [ ! -f $keyring_vault ]; then
-    echo "vault configuration not found"
-    return 0
-  fi
+    if [ ! -f "${keyring_vault}" ]; then
+        echo "vault configuration not found"
+        return 0
+    fi
 
-  if [ ! -f $sst_info ]; then
-    echo "SST info not found"
-    exit 1
-  fi
+    if [ ! -f "${sst_info}" ]; then
+        echo "SST info not found"
+        exit 1
+    fi
 
-  set +o xtrace # hide sensitive information
-  export VAULT_TOKEN=$(parse_ini "token" "${keyring_vault}")
-  export VAULT_ADDR=$(parse_ini "vault_url" "${keyring_vault}")
-  local vault_root=$(parse_ini "secret_mount_point" "${keyring_vault}")/backup
-  local transition_key=$(parse_ini "transition-key" "${sst_info}")
-  local gtid=$(parse_ini "galera-gtid" "${sst_info}")
+    set +o xtrace # hide sensitive information
+    local transition_key=$(parse_ini "transition-key" "${sst_info}")
+    if [ -z "${transition_key}" ]; then
+        echo "no transition key in the SST info: backup is an unencrypted, or it was already processed"
+        return 0
+    fi
 
-  if [ -z "${transition_key}" ]; then
-    echo "no transition key in the SST info: backup is an unencrypted, or it was already processed"
-    return 0
-  fi
+    export VAULT_TOKEN=$(parse_ini "token" "${keyring_vault}")
+    export VAULT_ADDR=$(parse_ini "vault_url" "${keyring_vault}")
+    local vault_root=$(parse_ini "secret_mount_point" "${keyring_vault}")/backup
+    local gtid=$(parse_ini "galera-gtid" "${sst_info}")
 
-  vault kv put ${vault_root}/${gtid} "transition_key=${transition_key}"
-  vault kv get "${vault_root}/${gtid}" >/dev/null
-  set -o xtrace
-  sed -i '/transition-key/d' $sst_info >/dev/null
+    curl \
+        -X PUT \
+        -H "X-Vault-Request: true" \
+        -H "X-Vault-Token: ${VAULT_TOKEN}" \
+        -d "{\"transition_key\":\"${transition_key}\"}" \
+        "${VAULT_ADDR}/v1/${vault_root}/backup/${gtid}"
+
+    set -o xtrace
+    sed -i '/transition-key/d' $sst_info >/dev/null
 }
