7265b7a1b23ca1b12e4e2781be18e2119bac4688
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-06-30 13:22:54 +0300

    K8SPXC-294 Rename configuration files

--- haproxy/dockerdir/entrypoint.sh
+++ haproxy/dockerdir/entrypoint.sh
@@ -5,10 +5,10 @@ set -o xtrace
 if [ "$1" = 'haproxy' ]; then
     haproxy_opt='-W -db -f /etc/haproxy/pxc/haproxy.cfg '
     cp /etc/haproxy/haproxy.cfg /etc/haproxy/pxc
-    if [ -f '/etc/haproxy-auto/haproxy-auto.cfg' ]; then
-        haproxy_opt+='-f /etc/haproxy-auto/haproxy-auto.cfg '
+    if [ -f '/etc/haproxy-custom/haproxy-global.cfg' ]; then
+        haproxy_opt+='-f /etc/haproxy-custom/haproxy-global.cfg '
     else
-        haproxy_opt+='-f /etc/haproxy/haproxy-auto.cfg '
+        haproxy_opt+='-f /etc/haproxy/haproxy-global.cfg '
     fi
     haproxy_opt+='-p /etc/haproxy/pxc/haproxy.pid '
 fi
