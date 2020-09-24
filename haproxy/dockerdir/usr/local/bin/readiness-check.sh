4cf122c0f114f89d458a85bb3c75f57edeafc1c5
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-09-24 14:31:01 +0300

    Update haproxy/dockerdir/usr/local/bin/readiness-check.sh
    Co-authored-by: Mykola Marzhan <mykola.marzhan@percona.com>

--- haproxy/dockerdir/usr/local/bin/readiness-check.sh
+++ haproxy/dockerdir/usr/local/bin/readiness-check.sh
@@ -8,9 +8,9 @@ TIMEOUT=10
 MYSQL_CMDLINE="/usr/bin/timeout $TIMEOUT /usr/bin/mysql -nNE -u$MONITOR_USER"
 
 { set +x; } 2>/dev/null
-MONITOR_PASSWORD=${MONITOR_PASSWORD:-$PATH}
-STATUS=$(MYSQL_PWD="${MONITOR_PASSWORD}" $MYSQL_CMDLINE -h127.0.0.1 -P$PXC_SERVER_PORT -e 'select 1;' | sed -n -e '2p' | tr '\n' ' ')
+export MYSQL_PWD=${MONITOR_PASSWORD:-$PATH}
 set -x
+STATUS=$($MYSQL_CMDLINE -h127.0.0.1 -P$PXC_SERVER_PORT -e 'select 1;' | sed -n -e '2p' | tr '\n' ' ')
 
 if [[ "${STATUS}" -eq 1 ]]; then
     exit 0
