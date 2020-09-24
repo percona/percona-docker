c13f09746f718a420cd9a896331dc6d6cf2c9ea7
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-09-24 16:19:50 +0300

    K8SPXC-462 improve readiness-check script

--- haproxy/dockerdir/usr/local/bin/readiness-check.sh
+++ haproxy/dockerdir/usr/local/bin/readiness-check.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-set -o xtrace
-
 PXC_SERVER_PORT='33062'
 MONITOR_USER='monitor'
 TIMEOUT=10
 MYSQL_CMDLINE="/usr/bin/timeout $TIMEOUT /usr/bin/mysql -nNE -u$MONITOR_USER"
 
-{ set +x; } 2>/dev/null
-MONITOR_PASSWORD=${MONITOR_PASSWORD:-$PATH}
-STATUS=$(MYSQL_PWD="${MONITOR_PASSWORD}" $MYSQL_CMDLINE -h127.0.0.1 -P$PXC_SERVER_PORT -e 'select 1;' | sed -n -e '2p' | tr '\n' ' ')
-set -x
+export MYSQL_PWD=${MONITOR_PASSWORD:-$PATH}
+STATUS=$($MYSQL_CMDLINE -h127.0.0.1 -P$PXC_SERVER_PORT -e 'select 1;' | sed -n -e '2p' | tr '\n' ' ')
 
 if [[ "${STATUS}" -eq 1 ]]; then
     exit 0
