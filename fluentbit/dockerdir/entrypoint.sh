55dfdfea8c3b3dc2ec624f77109d96a716390b79
Mykola Marzhan <mykola.marzhan@percona.com>
2020-11-05 13:15:59 +0200

    K8SPXC-485 avoid unnesesary coping
    coping can be an issue if security settings force readonly docker
    filesystem.

--- fluentbit/dockerdir/entrypoint.sh
+++ fluentbit/dockerdir/entrypoint.sh
@@ -4,10 +4,6 @@ set -o xtrace
 
 export PATH=$PATH:/opt/td-agent-bit/bin
 
-if [ -d '/etc/fluentbit/custom' ]; then
-    cp /etc/fluentbit/custom/*.conf /etc/fluentbit/
-fi
-
 if [ "$1" = 'fluent-bit' ]; then
     fluentbit_opt+='-c /etc/fluentbit/fluentbit.conf'
 fi
