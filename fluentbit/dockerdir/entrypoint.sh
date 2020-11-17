be9bd1ddfea4b005729e47cebd63f58f3b0e2735
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-11-17 14:31:22 +0200

    K8SPXC-485 add logrotation (#361)

--- fluentbit/dockerdir/entrypoint.sh
+++ fluentbit/dockerdir/entrypoint.sh
@@ -4,7 +4,19 @@ set -o xtrace
 
 export PATH=$PATH:/opt/td-agent-bit/bin
 
-if [ "$1" = 'fluent-bit' ]; then
-    fluentbit_opt+='-c /etc/fluentbit/fluentbit.conf'
+if [  "$1" = 'logrotate' ]; then
+    if [[ $EUID != 1001 ]]; then
+        # logrotate requires UID in /etc/passwd
+        sed -e "s^x:1001:^x:$EUID:^" /etc/passwd > /tmp/passwd
+        cat /tmp/passwd > /etc/passwd
+        rm -rf /tmp/passwd
+    fi
+    exec go-cron "0 0 * * *" logrotate -v -s /opt/percona/logrotate/logrotate.status /opt/percona/logrotate/logrotate-$SERVICE_TYPE.conf
+else
+    if [ "$1" = 'fluent-bit' ]; then
+        fluentbit_opt+='-c /etc/fluentbit/fluentbit.conf'
+    fi
+
+    exec "$@" $fluentbit_opt
 fi
-exec "$@" $fluentbit_opt
+
