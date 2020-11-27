ba54ccb5a98e37a72fadfdcf28d115df459ea227
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2020-11-27 10:38:32 +0200

    K8SPXC-485 flush error log only (#371)

--- fluentbit/dockerdir/usr/local/bin/postrotate-mysql.sh
+++ fluentbit/dockerdir/usr/local/bin/postrotate-mysql.sh
@@ -8,4 +8,4 @@ TIMEOUT=10
 MYSQL_CMDLINE="/usr/bin/timeout $TIMEOUT /usr/bin/mysql -nNE -u$MONITOR_USER"
 
 export MYSQL_PWD=${MONITOR_PASSWORD}
-$MYSQL_CMDLINE -h127.0.0.1 -P$PXC_SERVER_PORT -e 'select @@global.long_query_time into @lqt_save; set global long_query_time=2000; select sleep(2); FLUSH LOGS; select sleep(2); set global long_query_time=@lqt_save;'
+$MYSQL_CMDLINE -h127.0.0.1 -P$PXC_SERVER_PORT -e 'FLUSH ERROR LOGS;'
