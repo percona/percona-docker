7727fe9ab8f9108a513c21782bbce833dd156b1b
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2021-03-24 18:24:54 +0200

    K8SPG-24 add pgBadger docker image (#422)

---
+++ postgresql-containers/badger/badgerserver.go
@@ -0,0 +1,76 @@
+/*
+ Copyright 2016 - 2021 Crunchy Data Solutions, Inc.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+const REPORT = "/report/index.html"
+const PG_BADGER_SERVICE_PORT = "PGBADGER_SERVICE_PORT"
+const DEFAULT_PGBADGER_PORT = "10000"
+
+func init() {
+	log.SetOutput(os.Stdout)
+	log.Println("BadgerServer starting..")
+}
+
+func main() {
+	http.HandleFunc("/api/badgergenerate", BadgerGenerate)
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("/report"))))
+	http.HandleFunc("/", RootPathRedirect)
+	port := os.Getenv(PG_BADGER_SERVICE_PORT)
+	if port == "" {
+		log.Printf("PGBadger port not found. Using default %s\n", DEFAULT_PGBADGER_PORT)
+		port = DEFAULT_PGBADGER_PORT
+	}
+	log.Fatal(http.ListenAndServe(":" + port, nil))
+}
+
+// BadgerGenerate perform a pgbadger to create the HTML output file
+func BadgerGenerate(w http.ResponseWriter, r *http.Request) {
+	log.Println("Generating report..")
+
+	var cmd *exec.Cmd
+	cmd = exec.Command("badger-generate.sh")
+	var out bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	if err != nil {
+		errorMsg := fmt.Sprintf("Error running badger-generate: %s\n%s", err, stderr.String())
+		log.Println(errorMsg)
+		http.Error(w, errorMsg, http.StatusInternalServerError)
+		return
+	}
+
+	log.Println("Report generated.  Redirecting..")
+	http.Redirect(w, r, "/static", 302)
+}
+
+func RootPathRedirect(w http.ResponseWriter, r *http.Request) {
+	redirect_url := "/static/"
+	if _, err := os.Stat(REPORT); os.IsNotExist(err) {
+		redirect_url = "/api/badgergenerate"
+	}
+	http.Redirect(w, r, redirect_url, 302)
+}
---
+++ postgresql-containers/bin/pgbadger/badger-generate.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Copyright 2016 - 2021 Crunchy Data Solutions, Inc.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+CRUNCHY_DIR=${CRUNCHY_DIR:-'/opt/crunchy'}
+source "${CRUNCHY_DIR}/bin/common_lib.sh"
+enable_debugging
+export BADGER_CUSTOM_OPTS=${BADGER_CUSTOM_OPTS:-}
+
+TARGET=${HOSTNAME?}
+if [[ -v BADGER_TARGET ]]
+then
+    echo_info "BADGER_TARGET environment variable set.  Setting PGDATA target.."
+    TARGET=${BADGER_TARGET?}
+fi
+
+# The following command build-up is to avoid a bug where
+# BADGER_CUSTOM_OPTS might have quotes in the value.  Bash does
+# automatic escaping of quotes when found in a value which
+# breaks pgBadger.
+if [[ -f /tmp/cmd ]]
+then
+    rm -f /tmp/cmd
+fi
+
+echo -n "/bin/pgbadger -f stderr " >> /tmp/cmd
+echo -n "${BADGER_CUSTOM_OPTS?} " >> /tmp/cmd
+echo -n "-o /report/index.html /pgdata/${TARGET?}/pg_log/*.log"  >> /tmp/cmd
+
+echo_info "Creating pgBadger output.."
+source /tmp/cmd
