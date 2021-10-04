329682001579371d8590b79623d6c04838c6a9ec
Ege Güneş <ege.gunes@percona.com>
2021-10-04 16:55:52 +0300

    K8SPS-13: Add peer-list to orchestrator

---
+++ orchestrator/dockerdir/usr/bin/add_mysql_nodes.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+set -e
+set -o xtrace
+
+ORC_HOST=127.0.0.1:3000
+
+wait_for_leader() {
+    local retry=0
+    local leader=""
+
+    until [ ${leader} -ne "" ]; do
+        if [ ${retry} -gt 60 ]; then
+            echo "Waiting for leader failed after 60 attempts"
+            exit 1
+        fi
+
+        local leader=$(curl "${ORC_HOST}/api/raft-leader" 2>/dev/null)
+
+        retry=$(($retry + 1))
+        sleep 1
+    done
+}
+
+am_i_leader() {
+    local resp=$(curl -w httpcode=%{http_code} "${ORC_HOST}/api/leader-check" 2>/dev/null)
+    local http_code=$(echo "${resp}" | sed -e 's/.*\httpcode=//')
+
+    if [ ${http_code} -ne 200 ]; then
+        return 1
+    fi
+
+    return 0
+}
+
+discover() {
+    local host=$1
+    local port=$2
+
+    curl "${ORC_HOST}/api/discover/${host}/${port}"
+}
+
+main() {
+    # Wait for the leader election
+    wait_for_leader
+
+    # Exit if not master
+    if ! am_i_leader; then
+        echo "I'm not the leader. Exiting..."
+        exit 0
+    fi
+
+    # Discover
+    while read mysql_host; do
+        if [ -z "$mysql_host" ]; then
+            echo "Could not find PEERS ..."
+            exit 0
+        fi
+
+        discover "${mysql_host}" 3306
+    done
+}
+
+main
