2fa04e0cd030313254c44bacd1adcfaea3599a73
Andrii Dema <a.dema@jazzserve.com>
2021-12-08 13:59:08 +0200

    K8SPSMDB-608 fix: password of backup user is printed in backup agent logs
    https://jira.percona.com/browse/K8SPSMDB-608

--- percona-backup-mongodb/start-agent.sh
+++ percona-backup-mongodb/start-agent.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-set -o xtrace
-
-for argv
-do
-	if [[ -n "$usenext" ]]
-	then
+for argv; do
+	if [[ -n "$usenext" ]]; then
 		export PBM_MONGODB_URI="${argv}"
 		break
 	fi
-	if [[ "$argv" == '--mongodb-uri' ]]
-	then
+	if [[ "$argv" == '--mongodb-uri' ]]; then
 		use_next='true'
 		# TODO should we check if last?
 		continue
-	elif [[ "$argv" == '--mongodb-uri='* ]]
-	then
+	elif [[ "$argv" == '--mongodb-uri='* ]]; then
 		export PBM_MONGODB_URI="${argv#--mongodb-uri=}"
 		break
 	fi
 done
 
 # TODO should we check if all parts are set?
+set +o xtrace
 [[ -z "$PBM_MONGODB_URI" ]] && export PBM_MONGODB_URI="mongodb://${PBM_AGENT_MONGODB_USERNAME}:${PBM_AGENT_MONGODB_PASSWORD}@localhost:${PBM_MONGODB_PORT}/?replicaSet=${PBM_MONGODB_REPLSET}"
+set -o xtrace
 
 if [ "${1:0:9}" = "pbm-agent" ]; then
 	OUT="$(mktemp)"
@@ -33,11 +29,15 @@ if [ "${1:0:9}" = "pbm-agent" ]; then
 			echo "waiting for sharded scluster"
 
 			# check in case if shard has role 'shardsrv'
+			set +o xtrace
 			mongo "${PBM_MONGODB_URI}" --eval="db.isMaster().\$configServerState.opTime.ts" --quiet | tee "$OUT"
+			set -o xtrace
 			exit_status=$?
 
 			# check in case if shard has role 'configsrv'
+			set +o xtrace
 			mongo "${PBM_MONGODB_URI}" --eval="db.isMaster().configsvr" --quiet | tail -n 1 | tee "$OUT_CFG"
+			set -o xtrace
 			exit_status_cfg=$?
 
 			ts=$(grep -E '^Timestamp\([0-9]+, [0-9]+\)$' "$OUT")
@@ -49,7 +49,9 @@ if [ "${1:0:9}" = "pbm-agent" ]; then
 				sleep "$((timeout * i))"
 			fi
 		else
+			set +o xtrace
 			mongo "${PBM_MONGODB_URI}" --eval="(db.isMaster().hosts).length" --quiet | tee "$OUT"
+			set -o xtrace
 			exit_status=$?
 			rs_size=$(grep -E '^([0-9]+)$' "$OUT")
 			if [[ "${exit_status}" == 0 ]] && [[ $rs_size -ge 1 ]]; then
