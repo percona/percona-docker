aa37d21c08ac26dd080cef2bedbdca99aed1b034
Ege Güneş <ege.gunes@percona.com>
2022-08-16 17:49:18 +0300

    K8SPS-109: Add Dockerfile for MySQL Router (#628)
    * K8SPS-82: Add Dockerfile for MySQL Router
    
    * separate k8s entrypoint logic

---
+++ mysql-router/dockerdir/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+if [ -n "$KUBERNETES_SERVICE_HOST" ]; then
+	ROUTER_DIR=${ROUTER_DIR:-/tmp/router}
+	OPERATOR_USER=${OPERATOR_USER:-operator}
+	NAMESPACE=$(</var/run/secrets/kubernetes.io/serviceaccount/namespace)
+
+	if [ -f "/etc/mysql/mysql-users-secret/$OPERATOR_USER" ]; then
+		OPERATOR_PASS=$(</etc/mysql/mysql-users-secret/$OPERATOR_USER)
+	fi
+
+	mysqlrouter --force \
+		--bootstrap ${OPERATOR_USER}:${OPERATOR_PASS}@${MYSQL_SERVICE_NAME}-0.${MYSQL_SERVICE_NAME}.${NAMESPACE}.svc.cluster.local \
+		--conf-bind-address 0.0.0.0 \
+		--directory ${ROUTER_DIR}
+
+	sed -i 's/logging_folder=.*/logging_folder=/g' ${ROUTER_DIR}/mysqlrouter.conf
+	sed -i "/\[logger\]/a destination=/dev/stdout" ${ROUTER_DIR}/mysqlrouter.conf
+fi
+
+exec "$@"
