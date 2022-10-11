da48a333cd55ac4bac7264e11454cc44a16f4015
Andrii Dema <a.dema@jazzserve.com>
2022-10-11 16:20:53 +0300

    `shfmt`

--- percona-xtradb-cluster-8.0-backup/recovery-azure.sh
+++ percona-xtradb-cluster-8.0-backup/recovery-azure.sh
@@ -9,7 +9,7 @@ LIB_PATH='/usr/lib/pxc'
 
 INSECURE_ARG=""
 if [ -n "$VERIFY_TLS" ] && [[ $VERIFY_TLS == "false" ]]; then
-  INSECURE_ARG="--insecure"
+	INSECURE_ARG="--insecure"
 fi
 
 # temporary fix for PXB-2784
@@ -25,22 +25,22 @@ xbcloud get ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} "$BACKUP_PATH" --storage=azur
 set +o xtrace
 transition_key=$(vault_get $tmp/sst_info)
 if [[ -n $transition_key && $transition_key != null ]]; then
-    MYSQL_VERSION=$(parse_ini 'mysql-version' "$tmp/sst_info")
-    if compare_versions "$MYSQL_VERSION" '<' '5.7.29' &&
-        [[ $MYSQL_VERSION != '5.7.28-31-57.2' ]]; then
-         transition_key="\$transition_key"
-    fi
+	MYSQL_VERSION=$(parse_ini 'mysql-version' "$tmp/sst_info")
+	if compare_versions "$MYSQL_VERSION" '<' '5.7.29' \
+		&& [[ $MYSQL_VERSION != '5.7.28-31-57.2' ]]; then
+		transition_key='$transition_key'
+	fi
 
-    transition_option="--transition-key=$transition_key"
-    master_key_options="--generate-new-master-key"
-    echo transition-key exists
+	transition_option="--transition-key=$transition_key"
+	master_key_options="--generate-new-master-key"
+	echo transition-key exists
 fi
 
 echo "+ xtrabackup ${XB_USE_MEMORY+--use-memory=$XB_USE_MEMORY} --prepare --rollback-prepared-trx \
     --xtrabackup-plugin-dir=/usr/lib64/xtrabackup/plugin --target-dir=$tmp"
 
 xtrabackup ${XB_USE_MEMORY+--use-memory=$XB_USE_MEMORY} --prepare $transition_option --rollback-prepared-trx \
-    --xtrabackup-plugin-dir=/usr/lib64/xtrabackup/plugin --target-dir=$tmp
+	--xtrabackup-plugin-dir=/usr/lib64/xtrabackup/plugin --target-dir=$tmp
 
 echo "+ xtrabackup --defaults-group=mysqld --datadir=/datadir --move-back \
     --force-non-empty-directories $master_key_options \
@@ -48,8 +48,8 @@ echo "+ xtrabackup --defaults-group=mysqld --datadir=/datadir --move-back \
     --xtrabackup-plugin-dir=/usr/lib64/xtrabackup/plugin --target-dir=$tmp"
 
 xtrabackup --defaults-group=mysqld --datadir=/datadir --move-back \
-    --force-non-empty-directories $transition_option $master_key_options \
-    --keyring-vault-config=/etc/mysql/vault-keyring-secret/keyring_vault.conf --early-plugin-load=keyring_vault.so \
-    --xtrabackup-plugin-dir=/usr/lib64/xtrabackup/plugin --target-dir=$tmp
+	--force-non-empty-directories $transition_option $master_key_options \
+	--keyring-vault-config=/etc/mysql/vault-keyring-secret/keyring_vault.conf --early-plugin-load=keyring_vault.so \
+	--xtrabackup-plugin-dir=/usr/lib64/xtrabackup/plugin --target-dir=$tmp
 
 rm -rf $tmp
--- percona-xtradb-cluster-8.0-backup/run_backup.sh
+++ percona-xtradb-cluster-8.0-backup/run_backup.sh
@@ -11,188 +11,188 @@ LIB_PATH='/usr/lib/pxc'
 SOCAT_OPTS="TCP-LISTEN:4444,reuseaddr,retry=30"
 
 function check_ssl() {
-    CA=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-    if [ -f /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt ]; then
-        CA=/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt
-    fi
-    SSL_DIR=${SSL_DIR:-/etc/mysql/ssl}
-    if [ -f ${SSL_DIR}/ca.crt ]; then
-        CA=${SSL_DIR}/ca.crt
-    fi
-    SSL_INTERNAL_DIR=${SSL_INTERNAL_DIR:-/etc/mysql/ssl-internal}
-    if [ -f ${SSL_INTERNAL_DIR}/ca.crt ]; then
-        CA=${SSL_INTERNAL_DIR}/ca.crt
-    fi
+	CA=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+	if [ -f /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt ]; then
+		CA=/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt
+	fi
+	SSL_DIR=${SSL_DIR:-/etc/mysql/ssl}
+	if [ -f ${SSL_DIR}/ca.crt ]; then
+		CA=${SSL_DIR}/ca.crt
+	fi
+	SSL_INTERNAL_DIR=${SSL_INTERNAL_DIR:-/etc/mysql/ssl-internal}
+	if [ -f ${SSL_INTERNAL_DIR}/ca.crt ]; then
+		CA=${SSL_INTERNAL_DIR}/ca.crt
+	fi
 
-    KEY=${SSL_DIR}/tls.key
-    CERT=${SSL_DIR}/tls.crt
-    if [ -f ${SSL_INTERNAL_DIR}/tls.key -a -f ${SSL_INTERNAL_DIR}/tls.crt ]; then
-        KEY=${SSL_INTERNAL_DIR}/tls.key
-        CERT=${SSL_INTERNAL_DIR}/tls.crt
-    fi
+	KEY=${SSL_DIR}/tls.key
+	CERT=${SSL_DIR}/tls.crt
+	if [ -f ${SSL_INTERNAL_DIR}/tls.key -a -f ${SSL_INTERNAL_DIR}/tls.crt ]; then
+		KEY=${SSL_INTERNAL_DIR}/tls.key
+		CERT=${SSL_INTERNAL_DIR}/tls.crt
+	fi
 
-    if [ -f "$CA" -a -f "$KEY" -a -f "$CERT" ]; then
-        SOCAT_OPTS="openssl-listen:4444,reuseaddr,cert=${CERT},key=${KEY},cafile=${CA},verify=1,retry=30"
-    fi
+	if [ -f "$CA" -a -f "$KEY" -a -f "$CERT" ]; then
+		SOCAT_OPTS="openssl-listen:4444,reuseaddr,cert=${CERT},key=${KEY},cafile=${CA},verify=1,retry=30"
+	fi
 }
 
 FIRST_RECEIVED=0
 SST_FAILED=0
 function handle_sigterm() {
-    if (( $FIRST_RECEIVED == 0 )); then
-        pid_s=$(ps -C socat -o pid= || true)
-        if [ -n "${pid_s}" ]; then
-            log 'ERROR' 'SST request failed'
-            SST_FAILED=1
-            kill $pid_s
-            exit 1
-        else
-            log 'INFO' 'SST request was finished'
-        fi
-    fi
+	if ((FIRST_RECEIVED == 0)); then
+		pid_s=$(ps -C socat -o pid= || true)
+		if [ -n "${pid_s}" ]; then
+			log 'ERROR' 'SST request failed'
+			SST_FAILED=1
+			kill $pid_s
+			exit 1
+		else
+			log 'INFO' 'SST request was finished'
+		fi
+	fi
 }
 
 function backup_volume() {
-    BACKUP_DIR=${BACKUP_DIR:-/backup/$PXC_SERVICE-$(date +%F-%H-%M)}
-    if [ -d "$BACKUP_DIR" ]; then
-        rm -rf $BACKUP_DIR/{xtrabackup.*,sst_info}
-    fi
+	BACKUP_DIR=${BACKUP_DIR:-/backup/$PXC_SERVICE-$(date +%F-%H-%M)}
+	if [ -d "$BACKUP_DIR" ]; then
+		rm -rf $BACKUP_DIR/{xtrabackup.*,sst_info}
+	fi
 
-    mkdir -p "$BACKUP_DIR"
-    cd "$BACKUP_DIR" || exit
+	mkdir -p "$BACKUP_DIR"
+	cd "$BACKUP_DIR" || exit
 
-    log 'INFO' "Backup to $BACKUP_DIR was started"
+	log 'INFO' "Backup to $BACKUP_DIR was started"
 
-    socat -u "$SOCAT_OPTS" stdio | xbstream -x &
-    wait $!
+	socat -u "$SOCAT_OPTS" stdio | xbstream -x &
+	wait $!
 
-    log 'INFO' 'Socat was started'
+	log 'INFO' 'Socat was started'
 
-    FIRST_RECEIVED=1
-    if [[ $? -ne 0 ]]; then
-        log 'ERROR' 'Socat(1) failed'
-        log 'ERROR' 'Backup was finished unsuccessfully'
-        exit 1
-    fi
-    echo "[IINFO] Socat(1) returned $?"
-    vault_store $BACKUP_DIR/${SST_INFO_NAME}
+	FIRST_RECEIVED=1
+	if [[ $? -ne 0 ]]; then
+		log 'ERROR' 'Socat(1) failed'
+		log 'ERROR' 'Backup was finished unsuccessfully'
+		exit 1
+	fi
+	echo "[IINFO] Socat(1) returned $?"
+	vault_store $BACKUP_DIR/${SST_INFO_NAME}
 
-    if (( $SST_FAILED == 0 )); then
-        FIRST_RECEIVED=0
-        socat -u "$SOCAT_OPTS" stdio >xtrabackup.stream
-        FIRST_RECEIVED=1
-        if [[ $? -ne 0 ]]; then
-            log 'ERROR' 'Socat(2) failed'
-            log 'ERROR' 'Backup was finished unsuccessfully'
-            exit 1
-        fi
-        log 'INFO' "Socat(2) returned $?"
-    fi
+	if ((SST_FAILED == 0)); then
+		FIRST_RECEIVED=0
+		socat -u "$SOCAT_OPTS" stdio >xtrabackup.stream
+		FIRST_RECEIVED=1
+		if [[ $? -ne 0 ]]; then
+			log 'ERROR' 'Socat(2) failed'
+			log 'ERROR' 'Backup was finished unsuccessfully'
+			exit 1
+		fi
+		log 'INFO' "Socat(2) returned $?"
+	fi
 
-    stat xtrabackup.stream
-    if (($(stat -c%s xtrabackup.stream) < 5000000)); then
-        log 'ERROR' 'Backup is empty'
-        log 'ERROR' 'Backup was finished unsuccessfully'
-        exit 1
-    fi
-    md5sum xtrabackup.stream | tee md5sum.txt
+	stat xtrabackup.stream
+	if (($(stat -c%s xtrabackup.stream) < 5000000)); then
+		log 'ERROR' 'Backup is empty'
+		log 'ERROR' 'Backup was finished unsuccessfully'
+		exit 1
+	fi
+	md5sum xtrabackup.stream | tee md5sum.txt
 }
 
 function backup_s3() {
-    mc_add_bucket_dest
+	mc_add_bucket_dest
 
-    socat -u "$SOCAT_OPTS" stdio | xbstream -x -C /tmp &
-    wait $!
-    log 'INFO' 'Socat was started'
+	socat -u "$SOCAT_OPTS" stdio | xbstream -x -C /tmp &
+	wait $!
+	log 'INFO' 'Socat was started'
 
-    FIRST_RECEIVED=1
-    if [[ $? -ne 0 ]]; then
-        log 'ERROR' 'Socat(1) failed'
-        log 'ERROR' 'Backup was finished unsuccessfully'
-        exit 1
-    fi
-    vault_store /tmp/${SST_INFO_NAME}
+	FIRST_RECEIVED=1
+	if [[ $? -ne 0 ]]; then
+		log 'ERROR' 'Socat(1) failed'
+		log 'ERROR' 'Backup was finished unsuccessfully'
+		exit 1
+	fi
+	vault_store /tmp/${SST_INFO_NAME}
 
-    xbstream -C /tmp -c ${SST_INFO_NAME} \
-        | xbcloud put ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=s3 --parallel=10 --md5 --s3-bucket="$S3_BUCKET" "$S3_BUCKET_PATH.$SST_INFO_NAME" 2>&1 \
-        | (grep -v "error: http request failed: Couldn't resolve host name" || exit 1)
+	xbstream -C /tmp -c ${SST_INFO_NAME} \
+		| xbcloud put ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=s3 --parallel=10 --md5 --s3-bucket="$S3_BUCKET" "$S3_BUCKET_PATH.$SST_INFO_NAME" 2>&1 \
+		| (grep -v "error: http request failed: Couldn't resolve host name" || exit 1)
 
-    if (( $SST_FAILED == 0 )); then
-         FIRST_RECEIVED=0
-         socat -u "$SOCAT_OPTS" stdio  \
-            | xbcloud put ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=s3 --parallel=10 --md5 --s3-bucket="$S3_BUCKET" "$S3_BUCKET_PATH" 2>&1 \
-            | (grep -v "error: http request failed: Couldn't resolve host name" || exit 1)
-         FIRST_RECEIVED=1
-    fi
+	if ((SST_FAILED == 0)); then
+		FIRST_RECEIVED=0
+		socat -u "$SOCAT_OPTS" stdio \
+			| xbcloud put ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=s3 --parallel=10 --md5 --s3-bucket="$S3_BUCKET" "$S3_BUCKET_PATH" 2>&1 \
+			| (grep -v "error: http request failed: Couldn't resolve host name" || exit 1)
+		FIRST_RECEIVED=1
+	fi
 
-    mc -C /tmp/mc stat ${INSECURE_ARG} "dest/$S3_BUCKET/$S3_BUCKET_PATH.md5"
-    md5_size=$(mc -C /tmp/mc stat ${INSECURE_ARG} --json "dest/$S3_BUCKET/$S3_BUCKET_PATH.md5" | sed -e 's/.*"size":\([0-9]*\).*/\1/')
-    if [[ $md5_size =~ "Object does not exist" ]] || (($md5_size < 23000)); then
-         log 'ERROR' 'Backup is empty'
-         log 'ERROR' 'Backup was finished unsuccessfull'
-         exit 1
-    fi
+	mc -C /tmp/mc stat ${INSECURE_ARG} "dest/$S3_BUCKET/$S3_BUCKET_PATH.md5"
+	md5_size=$(mc -C /tmp/mc stat ${INSECURE_ARG} --json "dest/$S3_BUCKET/$S3_BUCKET_PATH.md5" | sed -e 's/.*"size":\([0-9]*\).*/\1/')
+	if [[ $md5_size =~ "Object does not exist" ]] || ((md5_size < 23000)); then
+		log 'ERROR' 'Backup is empty'
+		log 'ERROR' 'Backup was finished unsuccessfull'
+		exit 1
+	fi
 }
 
 azure_auth_header() {
-    params="$1"
-    request_date="$2"
-    hex_key="$(echo -n $AZURE_ACCESS_KEY | base64 -d -w0 | xxd -p -c256)"
-    headers="x-ms-date:$request_date\nx-ms-version:2021-06-08"
-    resource="/$AZURE_STORAGE_ACCOUNT/$AZURE_CONTAINER_NAME"
-    string_to_sign="GET\n\n\n\n\n\n\n\n\n\n\n\n${headers}\n${resource}\n${params}"
-    signature=$(printf "$string_to_sign" | openssl dgst -sha256 -mac HMAC -macopt "hexkey:$hex_key" -binary | base64 -w0)
-    echo "Authorization: SharedKey $AZURE_STORAGE_ACCOUNT:$signature"
+	params="$1"
+	request_date="$2"
+	hex_key="$(echo -n $AZURE_ACCESS_KEY | base64 -d -w0 | xxd -p -c256)"
+	headers="x-ms-date:$request_date\nx-ms-version:2021-06-08"
+	resource="/$AZURE_STORAGE_ACCOUNT/$AZURE_CONTAINER_NAME"
+	string_to_sign="GET\n\n\n\n\n\n\n\n\n\n\n\n${headers}\n${resource}\n${params}"
+	signature=$(printf "$string_to_sign" | openssl dgst -sha256 -mac HMAC -macopt "hexkey:$hex_key" -binary | base64 -w0)
+	echo "Authorization: SharedKey $AZURE_STORAGE_ACCOUNT:$signature"
 }
 
 is_object_exist_azure() {
-    object="$1"
-    connection_string="$ENDPOINT/$AZURE_CONTAINER_NAME?comp=list&restype=container"
-    request_date=$(LC_ALL=en_US.utf8 TZ=GMT date "+%a, %d %h %Y %H:%M:%S %Z")
-    header_version="x-ms-version: 2021-06-08"
-    header_date="x-ms-date: $request_date"
-    header_auth=$(azure_auth_header "comp:list\nrestype:container" "$request_date")
+	object="$1"
+	connection_string="$ENDPOINT/$AZURE_CONTAINER_NAME?comp=list&restype=container"
+	request_date=$(LC_ALL=en_US.utf8 TZ=GMT date "+%a, %d %h %Y %H:%M:%S %Z")
+	header_version="x-ms-version: 2021-06-08"
+	header_date="x-ms-date: $request_date"
+	header_auth=$(azure_auth_header "comp:list\nrestype:container" "$request_date")
 
-    res=$(curl -s -H "$header_version" -H "$header_date" -H "$header_auth" ${connection_string} | grep $object)
+	res=$(curl -s -H "$header_version" -H "$header_date" -H "$header_auth" ${connection_string} | grep $object)
 
-    if [[ ${#res} -ne 0 ]]; then
-        return 1
-    fi
+	if [[ ${#res} -ne 0 ]]; then
+		return 1
+	fi
 }
 
 function backup_azure() {
-    S3_BUCKET_PATH=${S3_BUCKET_PATH:-$PXC_SERVICE-$(date +%F-%H-%M)-xtrabackup.stream}
-    CURL_RET_ERRORS_ARG='--curl-retriable-errors=7'
-    ENDPOINT=${AZURE_ENDPOINT:-"https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net"}
+	S3_BUCKET_PATH=${S3_BUCKET_PATH:-$PXC_SERVICE-$(date +%F-%H-%M)-xtrabackup.stream}
+	CURL_RET_ERRORS_ARG='--curl-retriable-errors=7'
+	ENDPOINT=${AZURE_ENDPOINT:-"https://$AZURE_STORAGE_ACCOUNT.blob.core.windows.net"}
 
-    log 'INFO' "Backup to $ENDPOINT/$AZURE_CONTAINER_NAME/$BACKUP_PATH"
+	log 'INFO' "Backup to $ENDPOINT/$AZURE_CONTAINER_NAME/$BACKUP_PATH"
 
-    is_object_exist_azure "$BACKUP_PATH.$SST_INFO_NAME/" || xbcloud delete ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=azure "$BACKUP_PATH.$SST_INFO_NAME"
-    is_object_exist_azure "$BACKUP_PATH/" || xbcloud delete ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=azure "$BACKUP_PATH"
+	is_object_exist_azure "$BACKUP_PATH.$SST_INFO_NAME/" || xbcloud delete ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=azure "$BACKUP_PATH.$SST_INFO_NAME"
+	is_object_exist_azure "$BACKUP_PATH/" || xbcloud delete ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=azure "$BACKUP_PATH"
 
-    socat -u "$SOCAT_OPTS" stdio | xbstream -x -C /tmp &
-    wait $!
-    log 'INFO' 'Socat was started'
+	socat -u "$SOCAT_OPTS" stdio | xbstream -x -C /tmp &
+	wait $!
+	log 'INFO' 'Socat was started'
 
-    FIRST_RECEIVED=1
-    if [[ $? -ne 0 ]]; then
-        log 'ERROR' 'Socat(1) failed'
-        log 'ERROR' 'Backup was finished unsuccessfully'
-        exit 1
-    fi
-    vault_store /tmp/${SST_INFO_NAME}
+	FIRST_RECEIVED=1
+	if [[ $? -ne 0 ]]; then
+		log 'ERROR' 'Socat(1) failed'
+		log 'ERROR' 'Backup was finished unsuccessfully'
+		exit 1
+	fi
+	vault_store /tmp/${SST_INFO_NAME}
 
-    xbstream -C /tmp -c ${SST_INFO_NAME} \
-        | xbcloud put ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=azure --parallel=10 "$BACKUP_PATH.$SST_INFO_NAME" 2>&1 \
-        | (grep -v "error: http request failed: Couldn't resolve host name" || exit 1)
+	xbstream -C /tmp -c ${SST_INFO_NAME} \
+		| xbcloud put ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=azure --parallel=10 "$BACKUP_PATH.$SST_INFO_NAME" 2>&1 \
+		| (grep -v "error: http request failed: Couldn't resolve host name" || exit 1)
 
-    if (( $SST_FAILED == 0 )); then
-         FIRST_RECEIVED=0
-         socat -u "$SOCAT_OPTS" stdio  \
-            | xbcloud put ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=azure --parallel=10 "$BACKUP_PATH" 2>&1 \
-            | (grep -v "error: http request failed: Couldn't resolve host name" || exit 1)
-         FIRST_RECEIVED=1
-    fi
+	if ((SST_FAILED == 0)); then
+		FIRST_RECEIVED=0
+		socat -u "$SOCAT_OPTS" stdio \
+			| xbcloud put ${CURL_RET_ERRORS_ARG} ${INSECURE_ARG} --storage=azure --parallel=10 "$BACKUP_PATH" 2>&1 \
+			| (grep -v "error: http request failed: Couldn't resolve host name" || exit 1)
+		FIRST_RECEIVED=1
+	fi
 }
 
 check_ssl
@@ -200,14 +200,14 @@ check_ssl
 trap 'handle_sigterm' 15
 
 if [ -n "$S3_BUCKET" ]; then
-    backup_s3
+	backup_s3
 elif [ -n "$AZURE_CONTAINER_NAME" ]; then
-    backup_azure
+	backup_azure
 else
-    backup_volume
+	backup_volume
 fi
 
-if (( $SST_FAILED == 0 )); then
-    touch /tmp/backup-is-completed
+if ((SST_FAILED == 0)); then
+	touch /tmp/backup-is-completed
 fi
 exit $SST_FAILED
