401ecbbf7c425dc84c5718c71cf944fd2ce4107c
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2022-11-22 14:16:39 +0200

    K8SPXC-1005 improve backup image (#717)
    * update kubectl to v1.21.14
        * remove old backup.sh scripts
        * do not print unneeded info in log

--- percona-xtradb-cluster-8.0-backup/lib/pxc/backup.sh
+++ percona-xtradb-cluster-8.0-backup/lib/pxc/backup.sh
@@ -53,17 +53,20 @@ azure_auth_header_file() {
 	params="$1"
 	request_date="$2"
 
+	{ set +x; } 2>/dev/null
 	echo -n "$AZURE_ACCESS_KEY" | base64 -d -w0 | hexdump -ve '1/1 "%02x"' >"$hex_tmp"
 	headers="x-ms-date:$request_date\nx-ms-version:2021-06-08"
 	resource="/$AZURE_STORAGE_ACCOUNT/$AZURE_CONTAINER_NAME"
 	string_to_sign="GET\n\n\n\n\n\n\n\n\n\n\n\n${headers}\n${resource}\n${params}"
 	printf '%s' "$string_to_sign" | openssl dgst -sha256 -mac HMAC -macopt "hexkey:$(cat "$hex_tmp")" -binary | base64 -w0 >"$signature_tmp"
 	echo -n "Authorization: SharedKey $AZURE_STORAGE_ACCOUNT:$(cat "$signature_tmp")" >"$auth_header_tmp"
+	set -x
 	echo "$auth_header_tmp"
 }
 
 is_object_exist_azure() {
 	object="$1"
+	{ set +x; } 2>/dev/null
 	connection_string="$ENDPOINT/$AZURE_CONTAINER_NAME?comp=list&restype=container"
 	request_date=$(LC_ALL=en_US.utf8 TZ=GMT date "+%a, %d %h %Y %H:%M:%S %Z")
 	header_version="x-ms-version: 2021-06-08"
@@ -71,6 +74,7 @@ is_object_exist_azure() {
 	header_auth_file=$(azure_auth_header_file "comp:list\nrestype:container" "$request_date")
 
 	res=$(curl -s -H "$header_version" -H "$header_date" -H "@$header_auth_file" "${connection_string}" | grep "$object")
+	set -x
 
 	if [[ ${#res} -ne 0 ]]; then
 		return 1
