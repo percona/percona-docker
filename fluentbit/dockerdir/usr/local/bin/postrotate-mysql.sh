a3d4bbacddee353d7c4f3a7675a9054763df57c3
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2023-06-09 21:43:02 +0300

    K8SPXC-1255 flush audit log if plugin is loaded
        * update fluentbit to 2.1.4

--- fluentbit/dockerdir/usr/local/bin/postrotate-mysql.sh
+++ fluentbit/dockerdir/usr/local/bin/postrotate-mysql.sh
@@ -5,7 +5,14 @@ set -o errexit
 PXC_SERVER_PORT='3306'
 MONITOR_USER='monitor'
 TIMEOUT=10
-MYSQL_CMDLINE="/usr/bin/timeout $TIMEOUT /usr/bin/mysql -nNE -u$MONITOR_USER"
+MYSQL_CMDLINE="/usr/bin/timeout $TIMEOUT /usr/bin/mysql -nNE -u$MONITOR_USER -h127.0.0.1 -P$PXC_SERVER_PORT"
 
 export MYSQL_PWD=${MONITOR_PASSWORD}
-$MYSQL_CMDLINE -h127.0.0.1 -P$PXC_SERVER_PORT -e 'FLUSH ERROR LOGS;'
+
+# Check if the audit plugin is loaded
+audit_plugin_loaded=$($MYSQL_CMDLINE -e "SHOW PLUGINS" | grep -c 'audit_log' || true)
+if [ $audit_plugin_loaded -gt 0 ]; then
+    $MYSQL_CMDLINE -e 'FLUSH ERROR LOGS;SET GLOBAL audit_log_flush=1;'
+else
+    $MYSQL_CMDLINE -e 'FLUSH ERROR LOGS;'
+fi
