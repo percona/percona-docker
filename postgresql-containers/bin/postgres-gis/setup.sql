287a1fc81dbfda49cb674409127705e3d77c82fc
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2023-11-07 18:55:54 +0200

    fix gis build

---
+++ postgresql-containers/bin/postgres-gis/setup.sql
@@ -0,0 +1,33 @@
+SET application_name="container_setup";
+
+create extension postgis;
+create extension postgis_topology;
+create extension fuzzystrmatch;
+create extension postgis_tiger_geocoder;
+create extension pg_stat_statements;
+create extension pgaudit;
+
+alter user postgres password 'PG_ROOT_PASSWORD';
+
+create user "PG_PRIMARY_USER" with REPLICATION  PASSWORD 'PG_PRIMARY_PASSWORD';
+create user "PG_USER" with password 'PG_PASSWORD';
+
+create table primarytable (key varchar(20), value varchar(20));
+grant all on primarytable to "PG_PRIMARY_USER";
+
+create database "PG_DATABASE";
+
+grant all privileges on database "PG_DATABASE" to "PG_USER";
+
+\c "PG_DATABASE"
+
+create extension postgis;
+create extension postgis_topology;
+create extension fuzzystrmatch;
+create extension postgis_tiger_geocoder;
+create extension pg_stat_statements;
+create extension pgaudit;
+
+\c "PG_DATABASE" "PG_USER";
+
+create schema "PG_USER";
