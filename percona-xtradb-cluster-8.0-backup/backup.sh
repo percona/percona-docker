65871296224f7eac64abbe8450edb67abe8873ed
Andrii Dema <a.dema@jazzserve.com>
2024-07-10 20:22:19 +0300

    K8SPXC-1337: use /opt/percona/peer-list instead of building it (#990)
    * K8SPXC-1337: use /opt/percona/peer-list instead of building it
    
    https://perconadev.atlassian.net/browse/K8SPXC-1337
    
    * fix dockerfiles
    
    * revert changes for pxc and proxy images
    
    * update mc and kubectl

--- percona-xtradb-cluster-8.0-backup/backup.sh
+++ percona-xtradb-cluster-8.0-backup/backup.sh
@@ -9,97 +9,97 @@ LIB_PATH='/usr/lib/pxc'
 GARBD_OPTS=""
 
 function get_backup_source() {
-    CLUSTER_SIZE=$(peer-list -on-start=/usr/bin/get-pxc-state -service=$PXC_SERVICE 2>&1 \
-        | grep wsrep_cluster_size \
-        | sort \
-        | tail -1 \
-        | cut -d : -f 12)
+	CLUSTER_SIZE=$(/opt/percona/peer-list -on-start=/usr/bin/get-pxc-state -service=$PXC_SERVICE 2>&1 \
+		| grep wsrep_cluster_size \
+		| sort \
+		| tail -1 \
+		| cut -d : -f 12)
 
-    if [ -z "${CLUSTER_SIZE}" ]; then
-        exit 1
-    fi
+	if [ -z "${CLUSTER_SIZE}" ]; then
+		exit 1
+	fi
 
-    FIRST_NODE=$(peer-list -on-start=/usr/bin/get-pxc-state -service=$PXC_SERVICE 2>&1 \
-        | grep wsrep_ready:ON:wsrep_connected:ON:wsrep_local_state_comment:Synced:wsrep_cluster_status:Primary \
-        | sort -r \
-        | tail -1 \
-        | cut -d : -f 2 \
-        | cut -d . -f 1)
+	FIRST_NODE=$(/opt/percona/peer-list -on-start=/usr/bin/get-pxc-state -service=$PXC_SERVICE 2>&1 \
+		| grep wsrep_ready:ON:wsrep_connected:ON:wsrep_local_state_comment:Synced:wsrep_cluster_status:Primary \
+		| sort -r \
+		| tail -1 \
+		| cut -d : -f 2 \
+		| cut -d . -f 1)
 
-    SKIP_FIRST_POD='|'
-    if (( ${CLUSTER_SIZE:-0} > 1 )); then
-        SKIP_FIRST_POD="$FIRST_NODE"
-    fi
-    peer-list -on-start=/usr/bin/get-pxc-state -service=$PXC_SERVICE 2>&1 \
-        | grep wsrep_ready:ON:wsrep_connected:ON:wsrep_local_state_comment:Synced:wsrep_cluster_status:Primary \
-        | grep -v $SKIP_FIRST_POD \
-        | sort \
-        | tail -1 \
-        | cut -d : -f 2 \
-        | cut -d . -f 1
+	SKIP_FIRST_POD='|'
+	if ((${CLUSTER_SIZE:-0} > 1)); then
+		SKIP_FIRST_POD="$FIRST_NODE"
+	fi
+	/opt/percona/peer-list -on-start=/usr/bin/get-pxc-state -service=$PXC_SERVICE 2>&1 \
+		| grep wsrep_ready:ON:wsrep_connected:ON:wsrep_local_state_comment:Synced:wsrep_cluster_status:Primary \
+		| grep -v $SKIP_FIRST_POD \
+		| sort \
+		| tail -1 \
+		| cut -d : -f 2 \
+		| cut -d . -f 1
 }
 
 function check_ssl() {
-    CA=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-    if [ -f /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt ]; then
-        CA=/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt
-    fi
-    SSL_DIR=${SSL_DIR:-/etc/mysql/ssl}
-    if [ -f ${SSL_DIR}/ca.crt ]; then
-        CA=${SSL_DIR}/ca.crt
-    fi
-    SSL_INTERNAL_DIR=${SSL_INTERNAL_DIR:-/etc/mysql/ssl-internal}
-    if [ -f ${SSL_INTERNAL_DIR}/ca.crt ]; then
-        CA=${SSL_INTERNAL_DIR}/ca.crt
-    fi
+	CA=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+	if [ -f /var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt ]; then
+		CA=/var/run/secrets/kubernetes.io/serviceaccount/service-ca.crt
+	fi
+	SSL_DIR=${SSL_DIR:-/etc/mysql/ssl}
+	if [ -f ${SSL_DIR}/ca.crt ]; then
+		CA=${SSL_DIR}/ca.crt
+	fi
+	SSL_INTERNAL_DIR=${SSL_INTERNAL_DIR:-/etc/mysql/ssl-internal}
+	if [ -f ${SSL_INTERNAL_DIR}/ca.crt ]; then
+		CA=${SSL_INTERNAL_DIR}/ca.crt
+	fi
 
-    KEY=${SSL_DIR}/tls.key
-    CERT=${SSL_DIR}/tls.crt
-    if [ -f ${SSL_INTERNAL_DIR}/tls.key -a -f ${SSL_INTERNAL_DIR}/tls.crt ]; then
-        KEY=${SSL_INTERNAL_DIR}/tls.key
-        CERT=${SSL_INTERNAL_DIR}/tls.crt
-    fi
+	KEY=${SSL_DIR}/tls.key
+	CERT=${SSL_DIR}/tls.crt
+	if [ -f ${SSL_INTERNAL_DIR}/tls.key -a -f ${SSL_INTERNAL_DIR}/tls.crt ]; then
+		KEY=${SSL_INTERNAL_DIR}/tls.key
+		CERT=${SSL_INTERNAL_DIR}/tls.crt
+	fi
 
-    if [ -f "$CA" -a -f "$KEY" -a -f "$CERT" ]; then
-         GARBD_OPTS="socket.ssl_ca=${CA};socket.ssl_cert=${CERT};socket.ssl_key=${KEY};socket.ssl_cipher=;pc.weight=0;${GARBD_OPTS}"
-    fi
+	if [ -f "$CA" -a -f "$KEY" -a -f "$CERT" ]; then
+		GARBD_OPTS="socket.ssl_ca=${CA};socket.ssl_cert=${CERT};socket.ssl_key=${KEY};socket.ssl_cipher=;pc.weight=0;${GARBD_OPTS}"
+	fi
 }
 
 function request_streaming() {
-    local LOCAL_IP=$(hostname -i | sed -E 's/.*\b([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\b.*/\1/')
-    local NODE_NAME=$(get_backup_source)
+	local LOCAL_IP=$(hostname -i | sed -E 's/.*\b([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\b.*/\1/')
+	local NODE_NAME=$(get_backup_source)
 
-    if [ -z "$NODE_NAME" ]; then
-        peer-list -on-start=/usr/bin/get-pxc-state -service=$PXC_SERVICE
-        log 'ERROR' 'Cannot find node for backup'
-        log 'ERROR' 'Backup was finished unsuccessfull'
-        exit 1
-    fi
+	if [ -z "$NODE_NAME" ]; then
+		/opt/percona/peer-list -on-start=/usr/bin/get-pxc-state -service=$PXC_SERVICE
+		log 'ERROR' 'Cannot find node for backup'
+		log 'ERROR' 'Backup was finished unsuccessfull'
+		exit 1
+	fi
 
-    set +o errexit
-    log 'INFO' 'Garbd was started'
-        garbd \
-            --address "gcomm://$NODE_NAME.$PXC_SERVICE?gmcast.listen_addr=tcp://0.0.0.0:4567" \
-            --donor "$NODE_NAME" \
-            --group "$PXC_SERVICE" \
-            --options "$GARBD_OPTS" \
-            --sst "xtrabackup-v2:$LOCAL_IP:4444/xtrabackup_sst//1" \
-            --recv-script="/usr/bin/run_backup.sh"
-        EXID_CODE=$?
+	set +o errexit
+	log 'INFO' 'Garbd was started'
+	garbd \
+		--address "gcomm://$NODE_NAME.$PXC_SERVICE?gmcast.listen_addr=tcp://0.0.0.0:4567" \
+		--donor "$NODE_NAME" \
+		--group "$PXC_SERVICE" \
+		--options "$GARBD_OPTS" \
+		--sst "xtrabackup-v2:$LOCAL_IP:4444/xtrabackup_sst//1" \
+		--recv-script="/usr/bin/run_backup.sh"
+	EXID_CODE=$?
 
-    if [ -f '/tmp/backup-is-completed' ]; then
-        log 'INFO' 'Backup was finished successfully'
-        exit 0
-    fi
+	if [ -f '/tmp/backup-is-completed' ]; then
+		log 'INFO' 'Backup was finished successfully'
+		exit 0
+	fi
 
-    log 'ERROR' 'Backup was finished unsuccessfull'
+	log 'ERROR' 'Backup was finished unsuccessfull'
 
-    exit $EXID_CODE
+	exit $EXID_CODE
 }
 
 check_ssl
 if [ -n "${S3_BUCKET}" ]; then
-   clean_backup_s3
+	clean_backup_s3
 fi
 request_streaming
 
