a76dc330d8b08fe58a42c9a8aaae72969281bf08
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2024-10-08 13:16:27 +0300

    fix build

--- percona-xtradb-cluster-8.0-backup/run_backup.sh
+++ percona-xtradb-cluster-8.0-backup/run_backup.sh
@@ -90,13 +90,6 @@ backup_volume() {
 		log 'INFO' "Socat(2) returned $?"
 	fi
 
-	stat xtrabackup.stream
-	if (($(stat -c%s xtrabackup.stream) < 5000000)); then
-		log 'ERROR' 'Backup is empty'
-		log 'ERROR' 'Backup was finished unsuccessfully'
-		exit 1
-	fi
-	md5sum xtrabackup.stream | tee md5sum.txt
 }
 
 backup_s3() {
@@ -126,13 +119,6 @@ backup_s3() {
 		FIRST_RECEIVED=1
 	fi
 
-	aws $AWS_S3_NO_VERIFY_SSL s3 ls s3://$S3_BUCKET/$S3_BUCKET_PATH.md5
-	md5_size=$(aws $AWS_S3_NO_VERIFY_SSL --output json s3api list-objects --bucket "$S3_BUCKET" --prefix "$S3_BUCKET_PATH.md5" --query 'Contents[0].Size' | sed -e 's/.*"size":\([0-9]*\).*/\1/')
-	if [[ $md5_size =~ "Object does not exist" ]] || ((md5_size < 23000)) ; then
-		log 'ERROR' 'Backup is empty'
-		log 'ERROR' 'Backup was finished unsuccessfull'
-		exit 1
-	fi
 }
 
 backup_azure() {
