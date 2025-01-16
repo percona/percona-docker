bf75908e443032b6b45a2902d1d6bba4e5122c2f
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2025-01-16 15:59:43 +0200

    K8SPXC-1475 use aws cli (#1048)
    * use aws-cli
    
    * aws lib
    
    * add post_backup script
    
    * fix build
    
    * K8SPXC-1475 use aws cli
    
        * rename function
    
    * add aws cli for pxc 5.7 backups
    
    * add aws cli for PXC 8.4 backup
    
    * remove debug

--- percona-xtradb-cluster-8.4-backup/lib/pxc/backup.sh
+++ percona-xtradb-cluster-8.4-backup/lib/pxc/backup.sh
@@ -2,13 +2,15 @@
 
 set -o errexit
 
+LIB_PATH='/usr/lib/pxc'
+. ${LIB_PATH}/aws.sh
+
 SST_INFO_NAME=sst_info
 XBCLOUD_ARGS="--curl-retriable-errors=7 $XBCLOUD_EXTRA_ARGS"
-
 INSECURE_ARG=""
+
 if [ -n "$VERIFY_TLS" ] && [[ $VERIFY_TLS == "false" ]]; then
-	INSECURE_ARG="--insecure"
-	XBCLOUD_ARGS="${INSECURE_ARG} ${XBCLOUD_ARGS}"
+	XBCLOUD_ARGS="--insecure ${XBCLOUD_ARGS}"
 fi
 
 S3_BUCKET_PATH=${S3_BUCKET_PATH:-$PXC_SERVICE-$(date +%F-%H-%M)-xtrabackup.stream}
@@ -24,24 +26,8 @@ log() {
 	set -x
 }
 
-is_object_exist() {
-	local bucket="$1"
-	local object="$2"
-
-	if [[ -n "$(mc -C /tmp/mc ${INSECURE_ARG} --json ls "dest/$bucket/$object" | jq '.status')" ]]; then
-		return 1
-	fi
-}
-
-mc_add_bucket_dest() {
-	echo "+ mc -C /tmp/mc ${INSECURE_ARG} config host add dest ${ENDPOINT:-https://s3.amazonaws.com} ACCESS_KEY_ID SECRET_ACCESS_KEY "
-	{ set +x; } 2>/dev/null
-	mc -C /tmp/mc ${INSECURE_ARG} config host add dest "${ENDPOINT:-https://s3.amazonaws.com}" "$ACCESS_KEY_ID" "$SECRET_ACCESS_KEY"
-	set -x
-}
-
 clean_backup_s3() {
-	mc_add_bucket_dest
+	s3_add_bucket_dest
 
 	is_object_exist "$S3_BUCKET" "$S3_BUCKET_PATH.$SST_INFO_NAME" || xbcloud delete ${XBCLOUD_ARGS} --storage=s3 --s3-bucket="$S3_BUCKET" "$S3_BUCKET_PATH.$SST_INFO_NAME"
 	is_object_exist "$S3_BUCKET" "$S3_BUCKET_PATH/" || xbcloud delete ${XBCLOUD_ARGS} --storage=s3 --s3-bucket="$S3_BUCKET" "$S3_BUCKET_PATH"
