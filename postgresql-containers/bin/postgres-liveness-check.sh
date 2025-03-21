afaccb2e5637bf1d1dcc4b2ea4fcc784415cb57a
George Kechagias <geo.kechagias@gmail.com>
2025-03-21 15:40:40 +0200

    improve commands

--- postgresql-containers/bin/postgres-liveness-check.sh
+++ postgresql-containers/bin/postgres-liveness-check.sh
@@ -3,4 +3,20 @@
 PATRONI_PORT=8008
 PATRONI_HOST=localhost
 
-curl -k "https://${PATRONI_HOST}:${PATRONI_PORT}/liveness"
+recovery_file='tmp/sleep-forever'
+if [ -f "${recovery_file}" ]; then
+	set +o xtrace
+	echo "The $recovery_file file is detected, node is going to an infinite loop"
+	echo "If you want to exit from the infinite loop, remove the $recovery_file file"
+	while [ -f "${recovery_file}" ]; do
+		sleep 3
+	done
+	exit 0
+fi
+
+response=$(curl -s -o /dev/null -w "%{http_code}" -k "https://${PATRONI_HOST}:${PATRONI_PORT}/liveness")
+
+if [[ "$response" -eq 200 ]]; then
+    exit 0
+fi
+exit 1
