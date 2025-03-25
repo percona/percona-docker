5334959ec8d1abfd4f96d8741d28f5d6aa3635a2
George Kechagias <geo.kechagias@gmail.com>
2025-03-25 14:01:34 +0200

    run shfmt -bn -ci -s -w

--- postgresql-containers/bin/postgres-readiness-check.sh
+++ postgresql-containers/bin/postgres-readiness-check.sh
@@ -16,7 +16,7 @@ fi
 
 response=$(curl -s -o /dev/null -w "%{http_code}" -k "https://${PATRONI_HOST}:${PATRONI_PORT}/readiness")
 
-if [[ "$response" -eq 200 ]]; then
-    exit 0
+if [[ $response -eq 200 ]]; then
+	exit 0
 fi
 exit 1
