3e54957ac17f0b6ec4f0dd278842ccda989a76f6
George Kechagias <geo.kechagias@gmail.com>
2025-04-02 07:50:21 +0300

    cr: [[ instead of [

--- postgresql-containers/bin/postgres-readiness-check.sh
+++ postgresql-containers/bin/postgres-readiness-check.sh
@@ -5,7 +5,7 @@ PATRONI_HOST=localhost
 
 # /pgdata/ is already mounted to the pg database container with rw permissions
 recovery_file='/pgdata/sleep-forever'
-if [ -f "${recovery_file}" ]; then
+if [[ -f "${recovery_file}" ]]; then
 	set +o xtrace
 	echo "The $recovery_file file is detected, node entered an infinite sleep"
 	echo "If you want to exit from the infinite sleep, remove the $recovery_file file"
