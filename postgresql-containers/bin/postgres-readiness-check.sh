a1af36a5ae9435faa485a295bcfe055ec687730d
George Kechagias <geo.kechagias@gmail.com>
2025-04-02 12:17:12 +0300

    cr: remove sleep forever from the readiness probe, keep the script for consistency

--- postgresql-containers/bin/postgres-readiness-check.sh
+++ postgresql-containers/bin/postgres-readiness-check.sh
@@ -3,15 +3,6 @@
 PATRONI_PORT=8008
 PATRONI_HOST=localhost
 
-# /pgdata/ is already mounted to the pg database container with rw permissions
-recovery_file='/pgdata/sleep-forever'
-if [[ -f ${recovery_file} ]]; then
-	set +o xtrace
-	echo "The $recovery_file file is detected, node entered an infinite sleep"
-	echo "If you want to exit from the infinite sleep, remove the $recovery_file file"
-	exit 0
-fi
-
 response=$(curl -s -o /dev/null -w "%{http_code}" -k "https://${PATRONI_HOST}:${PATRONI_PORT}/readiness")
 
 if [[ $response -eq 200 ]]; then
